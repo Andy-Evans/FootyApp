62b744c520c6be11970b5e4fb0d10dd56bc56504
Andy[push] <andy.evans@outlook.com>
2015-03-01 08:30:27 +0000

    Created Api project

---
+++ FootyApi/Controllers/TeamController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using FootApi.Models;
+
+namespace FootApi.Controllers
+{
+    public class TeamController : ApiController
+    {
+        // GET: api/Team
+        public IEnumerable<Team> Get()
+        {
+            return new Team[] { new Team(1,"Tarvin AFC",1), new Team(2, "Sandbach",2)};
+        }
+
+        // GET: api/Team/5
+        public Team Get(int id)
+        {
+            return new Team(1, "Tarvin AFC", 4);
+        }
+
+        // POST: api/Team
+        public void Post([FromBody]Team team)
+        {
+            new TeamRepository().Add(team);
+        }
+
+        // PUT: api/Team/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Team/5
+        public void Delete(int id)
+        {
+        }
+    }
+
+    public interface ITeamRepository
+    {
+        void Add(Team team);
+    }
+
+    public class TeamRepository : ITeamRepository
+    {
+        public void Add(Team team)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ FootyApi/Models/Team.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FootApi.Models
+{
+    public class Team
+    {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public int Position { get; private set; }
+
+        public Team(int id, string name, int position)
+        {
+            Id = id;
+            Name = name;
+            Position = position;
+        }
+    }
+}
